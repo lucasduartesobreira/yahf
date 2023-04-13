93595818957958cf11b0309939d6f27d4899bb9a
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2023-04-13 00:10:20 -0300

    test: validate the functions implemented the runner trait

--- src/handler.rs
+++ src/handler.rs
@@ -126,6 +126,12 @@ where
 }
 
 mod improv {
+    use std::{marker::PhantomData, pin::Pin};
+
+    use async_trait::async_trait;
+    use futures::Future;
+    use serde::{de::DeserializeOwned, Serialize};
+
     #[derive(Debug)]
     pub struct SerdeError {
         body: String,
@@ -352,6 +358,128 @@ mod improv {
         R: Runner<(FnInput, Deserializer), (FnOutput, Serializer)>,
     {
         runner.call_runner(req).await
+    }
+
+    #[cfg(test)]
+    mod improv_tests {
+        use std::marker::PhantomData;
+
+        use async_std_test::async_test;
+        use serde::{Deserialize, Serialize};
+
+        use crate::handler::improv::Json;
+
+        use super::{encapsulate_runner, Request, Response};
+
+        #[derive(Deserialize, Serialize)]
+        struct SomeBodyType {
+            field: String,
+        }
+
+        async fn simple_handler(input: Request<SomeBodyType>) -> Response<SomeBodyType> {
+            let mut new_field = input.body().field.to_owned();
+            new_field.push_str(" - Ed Sheeran");
+
+            Response::new(SomeBodyType { field: new_field })
+        }
+
+        async fn unit_handler() -> Response<SomeBodyType> {
+            let new_field = String::from("HOPE - NF");
+
+            Response::new(SomeBodyType { field: new_field })
+        }
+
+        async fn simple_handler_with_body(input: SomeBodyType) -> Response<SomeBodyType> {
+            let mut new_field = input.field;
+            new_field.push_str(" - Halsey");
+
+            Response::new(SomeBodyType { field: new_field })
+        }
+
+        async fn handler_with_simple_body_on_input_and_output(input: SomeBodyType) -> SomeBodyType {
+            let mut new_field = input.field;
+            new_field.push_str(" - Imagine Dragons");
+
+            SomeBodyType { field: new_field }
+        }
+
+        #[async_test]
+        async fn test_simple_handler_implements_runner() -> std::io::Result<()> {
+            let a = encapsulate_runner(simple_handler, &Json(PhantomData), &Json(PhantomData));
+            let b = a(Request {
+                body: serde_json::json!({ "field": "South of the border" }).to_string(),
+            })
+            .await;
+
+            assert_eq!(
+                b.unwrap().body().as_str(),
+                serde_json::json!({ "field": "South of the border - Ed Sheeran"  }).to_string()
+            );
+
+            Ok(())
+        }
+
+        #[async_test]
+        async fn test_unit_handler_implements_runner() -> std::io::Result<()> {
+            let a = encapsulate_runner(unit_handler, &(), &Json(PhantomData));
+            let b = a(Request {
+                body: serde_json::json!({ "field": "South of the border" }).to_string(),
+            })
+            .await;
+
+            let expected_field_result = "HOPE - NF";
+
+            assert_eq!(
+                b.unwrap().body().as_str(),
+                serde_json::json!({ "field": expected_field_result }).to_string()
+            );
+
+            Ok(())
+        }
+
+        #[async_test]
+        async fn test_simple_handler_with_body_implements_runner() -> std::io::Result<()> {
+            let a = encapsulate_runner(
+                simple_handler_with_body,
+                &Json(PhantomData),
+                &Json(PhantomData),
+            );
+            let b = a(Request {
+                body: serde_json::json!({ "field": "So Good" }).to_string(),
+            })
+            .await;
+
+            let expected_field_result = "So Good - Halsey";
+
+            assert_eq!(
+                b.unwrap().body().as_str(),
+                serde_json::json!({ "field": expected_field_result }).to_string()
+            );
+
+            Ok(())
+        }
+
+        #[async_test]
+        async fn test_handler_with_simple_body_on_input_and_output_runner() -> std::io::Result<()> {
+            let a = encapsulate_runner(
+                handler_with_simple_body_on_input_and_output,
+                &Json(PhantomData),
+                &Json(PhantomData),
+            );
+            let b = a(Request {
+                body: serde_json::json!({ "field": "Sharks" }).to_string(),
+            })
+            .await;
+
+            let expected_field_result = "Sharks - Imagine Dragons";
+
+            assert_eq!(
+                b.unwrap().body().as_str(),
+                serde_json::json!({ "field": expected_field_result }).to_string()
+            );
+
+            Ok(())
+        }
     }
 }
 
