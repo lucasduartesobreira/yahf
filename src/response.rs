a03075f8b7bc96c36a741db31945ee08318d527e
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2023-04-15 12:47:44 -0300

    refactor: Response uses http::Response internally

--- src/response.rs
+++ src/response.rs
@@ -1,16 +1,20 @@
 use crate::handler::Result;
 
+pub type HttpResponse<T> = http::Response<T>;
+
 pub struct Response<T> {
-    body: T,
+    response: HttpResponse<T>,
 }
 
 impl<T> Response<T> {
     pub fn new(value: T) -> Self {
-        Self { body: value }
+        Self {
+            response: HttpResponse::new(value),
+        }
     }
 
     pub fn body(&self) -> &T {
-        &self.body
+        self.response.body()
     }
 
     // TODO: Valuate if this will keep this fn or move to an from_parts style
@@ -18,7 +22,9 @@ impl<T> Response<T> {
         self,
         callback: impl FnOnce(T) -> Result<BodyType>,
     ) -> Result<Response<BodyType>> {
-        let body = self.body;
-        callback(body).map(Response::<BodyType>::new)
+        let (parts, body) = self.response.into_parts();
+        callback(body).map(|body| Response {
+            response: HttpResponse::from_parts(parts, body),
+        })
     }
 }
