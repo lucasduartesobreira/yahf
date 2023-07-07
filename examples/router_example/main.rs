8017371325c49d696ca085f23fb07f5f59820811
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2023-07-06 21:25:13 -0300

    docs: create a example using Router to structure the application

---
+++ examples/router_example/main.rs
@@ -0,0 +1,61 @@
+extern crate yahf;
+
+use std::time;
+use std::time::UNIX_EPOCH;
+
+use serde::Deserialize;
+use serde::Serialize;
+use yahf::handler::Json;
+use yahf::handler::Result;
+use yahf::request::Request;
+
+use yahf::router::Router;
+use yahf::server::Server;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct ComputationBody {
+    value: u32,
+}
+
+async fn log_middleware(req: Result<Request<String>>) -> Result<Request<String>> {
+    match req.into_inner() {
+        Ok(req) => {
+            println!(
+                "{} - {} - {}",
+                time::SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .expect("Negative time")
+                    .as_millis(),
+                req.method().as_str(),
+                req.uri().path()
+            );
+
+            Ok(req).into()
+        }
+        Err(err) => Err(err).into(),
+    }
+}
+
+async fn first_computation(req: ComputationBody) -> ComputationBody {
+    ComputationBody {
+        value: req.value + 1,
+    }
+}
+
+async fn second_computation(req: ComputationBody) -> ComputationBody {
+    ComputationBody {
+        value: req.value * 2,
+    }
+}
+
+fn main() {
+    let mut router = Router::new().pre(log_middleware);
+    router.get("/first", first_computation, &Json::new(), &Json::new());
+
+    let mut server = Server::new();
+    server.get("/second", second_computation, &Json::new(), &Json::new());
+
+    let server = server.router(router);
+
+    server.listen("localhost:3000").unwrap();
+}
