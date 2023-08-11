67346555f1debad3066158df43d6e64302519b50
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2023-08-11 18:31:07 -0300

    refactor(response): make it a enum struct

--- src/response.rs
+++ src/response.rs
@@ -1,4 +1,4 @@
-use http::{HeaderMap, HeaderValue};
+use std::ops::{Deref, DerefMut};
 
 use crate::handler::InternalResult;
 
@@ -22,18 +22,41 @@ impl ResponseBuilder {
     }
 
     pub fn body<T>(self, body: T) -> Response<T> {
-        Response {
-            response: self
-                .builder
+        Response(
+            self.builder
                 .body(body)
                 .unwrap(),
-        }
+        )
     }
 }
 
 #[derive(Debug)]
-pub struct Response<T> {
-    response: HttpResponse<T>,
+pub struct Response<T>(HttpResponse<T>);
+
+impl<T> Deref for Response<T> {
+    type Target = http::Response<T>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> DerefMut for Response<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<T> From<http::Response<T>> for Response<T> {
+    fn from(value: http::Response<T>) -> Self {
+        Response(value)
+    }
+}
+
+impl<T> From<Response<T>> for http::Response<T> {
+    fn from(value: Response<T>) -> Self {
+        value.0
+    }
 }
 
 impl Response<()> {
@@ -46,13 +69,7 @@ impl Response<()> {
 
 impl<T> Response<T> {
     pub fn new(value: T) -> Self {
-        Self {
-            response: HttpResponse::new(value),
-        }
-    }
-
-    pub fn body(&self) -> &T {
-        self.response.body()
+        Self(HttpResponse::new(value))
     }
 
     // TODO: Valuate if this will keep this fn or move to an from_parts style
@@ -60,25 +77,11 @@ impl<T> Response<T> {
         self,
         callback: impl FnOnce(T) -> InternalResult<BodyType>,
     ) -> InternalResult<Response<BodyType>> {
-        let (parts, body) = self.response.into_parts();
-        callback(body).map(|body| Response {
-            response: HttpResponse::from_parts(parts, body),
-        })
-    }
-
-    pub fn status(&self) -> StatusCode {
-        self.response.status()
-    }
-
-    pub fn headers(&self) -> &HeaderMap<HeaderValue> {
-        self.response.headers()
-    }
-
-    pub fn headers_mut(&mut self) -> &mut HeaderMap<HeaderValue> {
-        self.response.headers_mut()
+        let (parts, body) = self.0.into_parts();
+        callback(body).map(|body| Response(HttpResponse::from_parts(parts, body)))
     }
 
     pub fn into_inner(self) -> HttpResponse<T> {
-        self.response
+        self.0
     }
 }
