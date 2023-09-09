5662bc193438a7ea4d8e78ebda498d21c0a6ebae
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2023-09-09 18:34:02 -0300

    test(docs): fix docs by adding () as bodyserializer

--- src/serializer.rs
+++ src/serializer.rs
@@ -30,3 +30,11 @@ impl BodySerializer for String {
         Ok(content.to_owned())
     }
 }
+
+impl BodySerializer for () {
+    type Item = ();
+
+    fn serialize(_content: Self::Item) -> InternalResult<StandardBodyType> {
+        Ok(String::with_capacity(0))
+    }
+}
