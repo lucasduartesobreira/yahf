dcd362b451f1441b82878ce0c302e8197ed24372
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2023-09-09 17:31:37 -0300

    style(test): fix some names

--- examples/hello_world/main.rs
+++ examples/hello_world/main.rs
@@ -1,19 +1,18 @@
 extern crate yahf;
 
-use std::net::SocketAddr;
-
 use yahf::server::Server;
 
 #[tokio::main]
 async fn main() {
-    let a = Server::new().get(
+    let server = Server::new().get(
         "/",
         || async { "Hello world".to_string() },
         &(),
         &String::with_capacity(0),
     );
 
-    a.listen(SocketAddr::from(([127, 0, 0, 1], 8000)))
+    server
+        .listen(([127, 0, 0, 1], 8000).into())
         .await
         .unwrap();
 }
